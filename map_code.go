0b3df3628724abb9da974d698118da956884ca7c
“rahulislamitibd” <itibdd@gmail.com>
2021-02-13 08:44:02 +0600

    Map_update

--- map_code.go
+++ map_code.go
@@ -2,10 +2,19 @@ package main
 import "fmt"
 func main(){
 //key=value
-x:=make (map [string]string)
-x["name"]="rahul"
-x["age"]="30"
-x["hight"]="5.5"
-delete(x,"age")
-fmt.Println(x)
+//x:=make (map [string]string)
+//x["name"]="rahul"
+//x["age"]="30"
+//x["hight"]="5.5"
+//delete(x,"age")
+//fmt.Println(x)
+var myMap map [string]int
+myMap=map[string]int{}
+myMap["a"]=12
+myMap["b"]=120
+myMap["c"]=1200
+myMap["d"]=12000
+myMap["e"]=120000
+fmt.Println(myMap)
+delete(myMap,"e")
 }
